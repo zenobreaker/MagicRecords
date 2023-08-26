96d19e3f5bf00aa84820c44fae0d230a67d1afa7
zeno <wpsh6369@gmail.com>
2023-08-26 12:01:45 +0900

    불러와서 적용하는 기능 작업중

--- Assets/9. Scripts/Managers/SaveManager.cs
+++ Assets/9. Scripts/Managers/SaveManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8dbf5b279b5f1cb06a7305c84dfe25fb6c1e65acbfdc855207c084530e0d6ef
-size 12874
+oid sha256:dd00d9fc03af860ad6ab80af1f16bd3a64e06ebda0545a1cd6b8eaddde6ce72a
+size 10552
