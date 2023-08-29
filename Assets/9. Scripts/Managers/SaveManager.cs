916138009775f27d32a055784b5af926954be018
zeno <wpsh6369@gmail.com>
2023-08-29 18:42:11 +0900

    게임 진행도 관련한 정보 저장
    레코드 및 스테이지 진행도 저장

--- Assets/9. Scripts/Managers/SaveManager.cs
+++ Assets/9. Scripts/Managers/SaveManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:695cea8eb6b5d8901b34b6b2ba0db3b8023f61a8d244478b4607359ca2ac4ce9
-size 13193
+oid sha256:06d9cfbde596ecbf848884b414cb1d649ea5cdae5ec7da00200e1eb4c91234c8
+size 15130
