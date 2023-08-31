6784006ce053e8a9ce29efdaac6a16fb32d84775
zeno <wpsh6369@gmail.com>
2023-08-31 19:05:07 +0900

    스킬 관련한 정리중

---
+++ Assets/9. Scripts/Character/BuffDebuff.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:633401972bb496c1c97922c0612f2d7c638e325ae7f4b625d19427daa12362d9
+size 1824
---
+++ Assets/9. Scripts/Skills/PassiveSkill.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b84f73b9089cb257f006833656afeb6b34aec4c2574907abac9a790160decef4
+size 314
